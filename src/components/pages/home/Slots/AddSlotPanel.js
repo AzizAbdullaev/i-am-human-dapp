22fe752155b9aaea0168e657746bb3b36f1d418c
Harjaap Dhillon <harjaapdhillon@Harjaaps-MacBook-Pro.local>
2023-05-02 01:21:22 +0530

    community-sbt-fixes

--- src/components/pages/home/Slots/AddSlotPanel.js
+++ src/components/pages/home/Slots/AddSlotPanel.js
@@ -1,23 +1,54 @@
-import { Fragment } from 'react';
-import { Dialog, Transition } from '@headlessui/react';
-import { XMarkIcon } from '@heroicons/react/24/outline';
+import { useState } from 'react';
 import { Panel } from '../../../common/panel';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 export function AddSlotPanel({ open, setOpen }) {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
   return (
     <Panel open={open} onClose={() => setOpen(false)} title="Add Slots">
       <form className="overflow-y-scroll w-full">
-        <div className="grid grid-cols-2">
+        <div className="grid grid-cols-2 gap-3 flex items-center">
           <div>
-            <input placeholder="FInal" />
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Slot Name
+              </label>
+              <div className="mt-1">
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  className="block p-3 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  placeholder="you@example.com"
+                />
+              </div>
+            </div>
           </div>
           <div>
-            <input placeholder="FInal" />
+            <DatePicker
+              selected={startDate}
+              onChange={(date) => setStartDate(date)}
+            />
           </div>
           <div>
-            <input placeholder="FInal" />
+            <DatePicker
+              selected={endDate}
+              onChange={(date) => setEndDate(date)}
+            />
           </div>
-          <div></div>
+          <div>
+            <button
+              type="button"
+              className="rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+            >
+              Button text
+            </button>
+          </div>
         </div>
       </form>
     </Panel>
